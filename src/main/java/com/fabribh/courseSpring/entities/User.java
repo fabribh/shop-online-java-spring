66ee994f32b947418d690748333f7f833581dd9f
Fabio Pedro Ribeiro <si.fabioribeiro@gmail.com>
2020-07-29 20:26:09 -0300

    Order, Instant, ISO 8601 part one

--- src/main/java/com/fabribh/courseSpring/entities/User.java
+++ src/main/java/com/fabribh/courseSpring/entities/User.java
@@ -1,13 +1,13 @@
 package com.fabribh.courseSpring.entities;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 @Entity
+@Table(name = "tb_user")
 public class User implements Serializable {
     private static final long serialVersionUID = 1L;
 
@@ -18,6 +18,9 @@ public class User implements Serializable {
     private String email;
     private String phone;
     private String password;
+
+    @OneToMany(mappedBy = "client")
+    private List<Order> orders = new ArrayList<>();
 
     public User(){
     }
@@ -81,5 +84,9 @@ public class User implements Serializable {
     @Override
     public int hashCode() {
         return Objects.hash(id);
+    }
+
+    public List<Order> getOrders() {
+        return orders;
     }
 }
