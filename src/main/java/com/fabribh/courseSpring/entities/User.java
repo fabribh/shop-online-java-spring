15b0898e5349714c2c0c42c470c2a26c8c009c10
Fabio Pedro Ribeiro <si.fabioribeiro@gmail.com>
2020-07-29 02:38:07 -0300

    H2 database, test profile, JPA

--- src/main/java/com/fabribh/courseSpring/entities/User.java
+++ src/main/java/com/fabribh/courseSpring/entities/User.java
@@ -1,10 +1,18 @@
 package com.fabribh.courseSpring.entities;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.io.Serializable;
 import java.util.Objects;
 
+@Entity
 public class User implements Serializable {
     private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String email;
