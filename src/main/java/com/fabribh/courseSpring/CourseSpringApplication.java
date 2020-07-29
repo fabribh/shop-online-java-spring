1d59293282a987710296fdcd6759315493431ab4
Fabio Pedro Ribeiro <si.fabioribeiro@gmail.com>
2020-07-29 01:34:34 -0300

    Project Created

---
+++ src/main/java/com/fabribh/courseSpring/CourseSpringApplication.java
@@ -0,0 +1,13 @@
+package com.fabribh.courseSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseSpringApplication.class, args);
+	}
+
+}
