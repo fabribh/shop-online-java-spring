73381038acf3bd37345b34bdf2d74402f61eb157
Fabio Pedro Ribeiro <si.fabioribeiro@gmail.com>
2020-07-30 21:57:02 -0300

    OrderItem, many-to-many association with extra attributes parte one

---
+++ src/main/java/com/fabribh/shopOnline/entities/pk/OrderItemPk.java
@@ -0,0 +1,54 @@
+package com.fabribh.shopOnline.entities.pk;
+
+import com.fabribh.shopOnline.entities.Order;
+import com.fabribh.shopOnline.entities.Product;
+
+import javax.persistence.Embeddable;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import java.io.Serializable;
+import java.util.Objects;
+
+@Embeddable
+public class OrderItemPk implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @ManyToOne
+    @JoinColumn(name = "product_id")
+    private Product product;
+
+    @ManyToOne
+    @JoinColumn(name = "category_id")
+    private Order order;
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+
+    public Order getOrder() {
+        return order;
+    }
+
+    public void setOrder(Order order) {
+        this.order = order;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        OrderItemPk that = (OrderItemPk) o;
+        return product.equals(that.product) &&
+                order.equals(that.order);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(product, order);
+    }
+}
