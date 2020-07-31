163f99dd59419dd893c8d99b766c6872ea27a982
Fabio Pedro Ribeiro <si.fabioribeiro@gmail.com>
2020-07-31 14:15:49 -0300

    Payment, one-to-one association

--- src/main/java/com/fabribh/shopOnline/config/TestConfig.java
+++ src/main/java/com/fabribh/shopOnline/config/TestConfig.java
@@ -70,5 +70,9 @@ public class TestConfig implements CommandLineRunner {
         OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 
         orderItemRespository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+
+        Payment pay1 = new Payment(null, Instant.parse("2020-06-20T21:53:07Z"),o1);
+        o1.setPayment(pay1);
+        orderRespository.save(o1);
     }
 }
