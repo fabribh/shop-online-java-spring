982a801424f6315689711feb7436efab3c4efbad
Fabio Pedro Ribeiro <si.fabioribeiro@gmail.com>
2020-08-02 13:50:42 -0300

    Exception handling - findingById

---
+++ src/main/java/com/fabribh/shopOnline/resources/exceptions/StandardError.java
@@ -0,0 +1,68 @@
+package com.fabribh.shopOnline.resources.exceptions;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import java.io.Serializable;
+import java.time.Instant;
+
+public class StandardError implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT")
+    private Instant timeStamp;
+    private Integer status;
+    private String error;
+    private String message;
+    private String path;
+
+    public StandardError() {
+    }
+
+    public StandardError(Instant timeStamp, Integer status, String error, String message, String path) {
+        this.timeStamp = timeStamp;
+        this.status = status;
+        this.error = error;
+        this.message = message;
+        this.path = path;
+    }
+
+    public Instant getTimeStamp() {
+        return timeStamp;
+    }
+
+    public void setTimeStamp(Instant timeStamp) {
+        this.timeStamp = timeStamp;
+    }
+
+    public Integer getStatus() {
+        return status;
+    }
+
+    public void setStatus(Integer status) {
+        this.status = status;
+    }
+
+    public String getError() {
+        return error;
+    }
+
+    public void setError(String error) {
+        this.error = error;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    public void setPath(String path) {
+        this.path = path;
+    }
+}
--- src/main/java/com/fabribh/shopOnline/services/UserService.java
+++ src/main/java/com/fabribh/shopOnline/services/UserService.java
@@ -2,6 +2,7 @@ package com.fabribh.shopOnline.services;
 
 import com.fabribh.shopOnline.entities.User;
 import com.fabribh.shopOnline.repositories.UserRespository;
+import com.fabribh.shopOnline.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -20,7 +21,7 @@ public class UserService {
 
     public User findById(Long id) {
         Optional<User> user = respository.findById(id);
-        return user.get();
+        return user.orElseThrow(() -> new ResourceNotFoundException(id));
     }
 
     public User insert(User obj) {
