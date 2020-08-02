9cb55e24fd6aea7101bac8fa2e2b856903e53f97
Fabio Pedro Ribeiro <si.fabioribeiro@gmail.com>
2020-08-02 13:20:50 -0300

    User update

--- src/main/java/com/fabribh/shopOnline/services/UserService.java
+++ src/main/java/com/fabribh/shopOnline/services/UserService.java
@@ -30,4 +30,16 @@ public class UserService {
     public void delete(Long id) {
         respository.deleteById(id);
     }
+
+    public User update(Long id, User obj) {
+        User entity = respository.getOne(id);
+        updateData(entity, obj);
+        return respository.save(entity);
+    }
+
+    private void updateData(User entity, User obj) {
+        entity.setName(obj.getName());
+        entity.setEmail(obj.getEmail());
+        entity.setPhone(obj.getPhone());
+    }
 }
