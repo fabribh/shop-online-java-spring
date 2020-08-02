d7f0003c29dca108974d9acc34507d6f657eb130
Fabio Pedro Ribeiro <si.fabioribeiro@gmail.com>
2020-08-02 14:07:14 -0300

    Exception handling - delete

--- src/main/java/com/fabribh/shopOnline/services/UserService.java
+++ src/main/java/com/fabribh/shopOnline/services/UserService.java
@@ -2,8 +2,11 @@ package com.fabribh.shopOnline.services;
 
 import com.fabribh.shopOnline.entities.User;
 import com.fabribh.shopOnline.repositories.UserRespository;
+import com.fabribh.shopOnline.services.exceptions.DatabaseException;
 import com.fabribh.shopOnline.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -29,7 +32,13 @@ public class UserService {
     }
 
     public void delete(Long id) {
-        respository.deleteById(id);
+        try {
+            respository.deleteById(id);
+        } catch (EmptyResultDataAccessException e) {
+            throw new ResourceNotFoundException(id);
+        } catch (DataIntegrityViolationException e) {
+            throw new DatabaseException(e.getMessage());
+        }
     }
 
     public User update(Long id, User obj) {
